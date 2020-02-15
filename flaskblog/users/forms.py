3799d6e7fe38fe3250ddcc6a39e029bd53bee592
unknown <parthsujalshah@gmail.com.com>
2020-02-15 21:25:26 +0530

    deployable

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError, TextAreaField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-from flaskblog.models import User
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm): 
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -44,11 +44,6 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is already taken. Please try again with a differrent one.')
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from flaskblog import app # imports from __init__.py file
+from flaskblog import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
