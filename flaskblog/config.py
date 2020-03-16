a68d4c4c9b3641005f647df300c4b02411dd9715
unknown <parthsujalshah@gmail.com.com>
2020-03-16 12:30:59 +0530

    changes made

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,8 +1,10 @@
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 class Config():
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:''@localhost/a'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
